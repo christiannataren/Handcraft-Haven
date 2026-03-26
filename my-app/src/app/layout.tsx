7cefab36a358233b22848e7ae06213de66cde1ec
jordansanchez23 <joivisvis@hotmail.com>
2026-03-25 20:50:32 -0700

    temporary array

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -32,7 +32,6 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <Navbar />
         {children}
-        
         <Footer />
       </body>
     </html>
--- my-app/src/app/ui/mostPopularProducts.tsx
+++
@@ -1,1 +0,0 @@
-import cards from '/data/cards'
