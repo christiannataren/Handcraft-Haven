2fe3a2c45d94b1098877082da7168c3cabeefbe3
jordansanchez23 <joivisvis@hotmail.com>
2026-03-31 17:45:14 -0700

    setting up database half way

---
+++ my-app/src/app/api/init/route.ts
@@ -0,0 +1,29 @@
+import { sql } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    await sql`
+      CREATE TABLE IF NOT EXISTS products (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(150) NOT NULL,
+        description TEXT NOT NULL,
+        price NUMERIC(10,2) NOT NULL,
+        image VARCHAR(255) NOT NULL,
+        category VARCHAR(100) NOT NULL,
+        is_popular BOOLEAN DEFAULT FALSE,
+        created_at TIMESTAMP DEFAULT NOW()
+      );
+    `;
+
+    return NextResponse.json({
+      message: "Products table created successfully",
+    });
+  } catch (error) {
+    console.error("Error creating products table:", error);
+    return NextResponse.json(
+      { error: "Failed to create products table" },
+      { status: 500 }
+    );
+  }
+}
